3cd342525ef8ecb021e95eff26474d762a58358b
bala-amypo <bala@amypo.in>
2025-12-19 04:59:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.EmployeeProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeProfileRepository extends JpaRepository<EmployeeProfile, Long> {
+    Optional<EmployeeProfile> findByEmployeeId(String employeeId);
+    Optional<EmployeeProfile> findByEmail(String email);
+    List<EmployeeProfile> findAll();
+}
