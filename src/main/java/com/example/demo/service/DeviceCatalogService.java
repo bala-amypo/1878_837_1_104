d22a64b1f8305cba07a5841bd7fc010402d6b1a6
bala-amypo <bala@amypo.in>
2025-12-22 00:49:17 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/DeviceCatalogService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.model.DeviceCatalogItem;
+
+import java.util.List;
+
+public interface DeviceCatalogService {
+    DeviceCatalogItem createItem(DeviceCatalogItem item);
+    List<DeviceCatalogItem> getAllItems();
+    DeviceCatalogItem updateActiveStatus(Long id, boolean active);
+}
