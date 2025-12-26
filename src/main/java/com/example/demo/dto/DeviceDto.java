5806962c083e3eb293cdaf86ae258f3be1e300f8
bala-amypo <bala@amypo.in>
2025-12-26 04:38:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/DeviceDto.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeviceDto {
+    private String deviceCode;
+    private String deviceType;
+    private String model;
+    private Integer maxAllowedPerEmployee;
+    private Boolean active;
+}
---
+++ src/main/java/com/example/demo/dto/EmployeeDto.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+    private String employeeId;
+    private String fullName;
+    private String email;
+    private String department;
+    private String jobRole;
+    private Boolean active;
+}
